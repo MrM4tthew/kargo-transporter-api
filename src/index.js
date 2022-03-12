c47d9b58b4e0cbf23f2c94c72199f4b7f8669d17
Matthew <tedja2727@gmail.com>
2022-03-12 12:04:46 +0700

    second commit

--- src/index.js
+++ src/index.js
@@ -1,36 +1,19 @@
-// ./src/index.js
-
-// importing the dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
-const helmet = require("helmet");
-const morgan = require("morgan");
-
-// defining the Express app
 const app = express();
+const port = 4000;
 
-// defining an array to work as the database (temporary solution)
-const ads = [{ title: "Hello, world (again)!" }];
-
-// adding Helmet to enhance your API's security
-app.use(helmet());
-
-// using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-// enabling CORS for all requests
-app.use(cors());
-
-// adding morgan to log HTTP requests
-app.use(morgan("combined"));
-
-// defining an endpoint to return all ads
-app.get("/", (req, res) => {
-  res.send(ads);
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
 });
 
-// starting the server
-app.listen(4000, () => {
-  console.log("listening on port 4000");
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
 });
